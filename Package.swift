cfe9d83939dfbd24b884514d2b62fd5f65e12ec5
Jvoe16 <56768946+Jvoe16@users.noreply.github.com>
2020-02-18 10:50:11 -0700

    Create Package.swift
    SwiftPM Compatibility

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "RichEditorView",
+    platforms: [.iOS(.v8), .tvOS(.v9)],
+    products: [
+        .library(name: "RichEditorView", targets: ["RichEditorView"])
+    ],
+    targets: [
+        .target(name: "RichEditorView")
+    ]
+)
